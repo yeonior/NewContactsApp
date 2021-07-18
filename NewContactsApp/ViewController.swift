c58a6c7dba9c10f6b70770dd4b29d2f33cb3c8b8
Rus <de.mr44@gmail.com>
2021-07-18 19:55:02 +0500

    Initial commit

---
+++ NewContactsApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NewContactsApp
+//
+//  Created by ruslan on 18.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
