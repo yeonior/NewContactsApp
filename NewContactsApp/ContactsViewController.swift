7391c3fab33ce56e3202a655b2a88642c649c6fb
Rus <de.mr44@gmail.com>
2021-07-18 21:57:12 +0500

    Added UITabBarController

---
+++ NewContactsApp/ContactsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsViewController.swift
+//  NewContactsApp
+//
+//  Created by ruslan on 18.07.2021.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+    }
+}
