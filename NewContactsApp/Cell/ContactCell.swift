5f229a50ace0051e663009f7f3ad21fc6401accf
Rus <de.mr44@gmail.com>
2021-07-24 14:29:35 +0500

    Updated cells

--- NewContactsApp/Cell/ContactCell.swift
+++ NewContactsApp/Cell/ContactCell.swift
@@ -11,16 +11,68 @@ class ContactCell: UICollectionViewCell, SelfConfiguringCell {
     
     static var reuseId: String = "ContactCell"
     
+    let imageView = UIImageView()
+    let nameLabel = UILabel()
+    let phoneImageView = UIImageView()
+    let videoImageView = UIImageView()
+    let mailImageView = UIImageView()
+    
     var user: ContactsModel.User?
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .green
     }
     
     func configure(with user: ContactsModel.User) {
+        
         self.user = user
+        
+        backgroundColor = .secondarySystemBackground
+        
+        imageView.image = UIImage(named: user.imageString)
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 12.5
+        
+        nameLabel.text = user.fullname
+        nameLabel.font = UIFont.sfProRounded(ofSize: 18, weight: .medium)
+        nameLabel.textAlignment = .left
+        
+        let stackVIew = UIStackView(arrangedSubviews: [phoneImageView, videoImageView, mailImageView])
+        phoneImageView.image = #imageLiteral(resourceName: "phone")
+        videoImageView.image = #imageLiteral(resourceName: "solid")
+        mailImageView.image = #imageLiteral(resourceName: "mail")
+        
+        stackVIew.axis = .horizontal
+        stackVIew.distribution = .fillEqually
+        stackVIew.spacing = 5
+        
+        addSubview(imageView)
+        addSubview(nameLabel)
+        addSubview(stackVIew)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackVIew.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 25),
+            imageView.heightAnchor.constraint(equalToConstant: 25)
+        ])
+        
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 16),
+            nameLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            nameLabel.trailingAnchor.constraint(lessThanOrEqualTo: stackVIew.leadingAnchor, constant: -16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackVIew.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            stackVIew.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
     }
     
     override func layoutSubviews() {
--- NewContactsApp/Cell/ProfileCell.swift
+++ NewContactsApp/Cell/ProfileCell.swift
@@ -10,15 +10,59 @@ import UIKit
 class ProfileCell: UICollectionViewCell, SelfConfiguringCell {
     
     static var reuseId: String = "ProfileCell"
+    
+    let imageView = UIImageView()
+    let nameLabel = UILabel()
+    let cardLabel = UILabel()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .orange
     }
     
     func configure(with user: ContactsModel.User) {
         
+        backgroundColor = .systemBackground
+        
+        imageView.image = UIImage(named: user.imageString)
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 29
+
+        nameLabel.text = user.fullname
+        nameLabel.font = UIFont.sfProRounded(ofSize: 28, weight: .semibold)
+        nameLabel.textAlignment = .left
+
+        cardLabel.text = "My card"
+        cardLabel.font = UIFont.sfProRounded(ofSize: 16, weight: .regular)
+        cardLabel.textAlignment = .left
+        cardLabel.textColor = #colorLiteral(red: 0.5019607843, green: 0.5019607843, blue: 0.5019607843, alpha: 1)
+
+        addSubview(imageView)
+        addSubview(nameLabel)
+        addSubview(cardLabel)
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        cardLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 58),
+            imageView.heightAnchor.constraint(equalToConstant: 58)
+        ])
+
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 16),
+            nameLabel.topAnchor.constraint(equalTo: topAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16)
+        ])
+
+        NSLayoutConstraint.activate([
+            cardLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 16),
+            cardLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 3)
+        ])
     }
     
     required init?(coder: NSCoder) {
